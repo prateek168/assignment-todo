6548f2650b3fbb8885b18f15ec78b594dfc2cf30
Prateek Verma <prateekverma1681998@gmail.com>
2025-07-08 12:41:37 +0530

    initial backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import session from "express-session";
+import passport from "./config/passport";
+import authRoutes from "./routes/auth.routes";
+import notesRoutes from "./routes/notes.routes";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express();
+
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+app.use(express.json()); 
+app.use(passport.initialize()); 
+
+app.use("/api/auth", authRoutes);
+app.use('/api/notes',notesRoutes)
+app.use(session({
+  secret: process.env.SESSION_SECRET!,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+export default app;
